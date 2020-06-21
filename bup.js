755d14c1a7694b4f7e99cb8ed0bc0ddda1bf1510
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-06-21 10:33:37 -0700

    Added competed function

--- bup.js
+++ bup.js
@@ -1,7 +1,7 @@
-
 var Word = require("./word.js");
 var inquirer = require("inquirer");
 var chalk = require('chalk');
+
 var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRS";
 
 //   List of the 15 most populous countries with a density of population  
@@ -22,39 +22,43 @@ var wordChoices = [
     "egypt",
     "vietnam"
 ];
-//Random the word answer
-var randomWordChoices = wordChoices[Math.floor(Math.random() * wordChoices.length)];
-//Pass random word through word constructor
-var computerWord = new Word(randomWordChoices);
-var requireNewWord = false;
 
+
+// Array for guessed letters
 // Array for guessed letters
 var incorrectLetters = [];
 var correctLetters = [];
 
 // Guesses left
-var guessesLeft = 10;
+var guessesLeft = 3;
 
-// Start the Game
 function startGame() {
   console.log(chalk.yellow("\n=================================================================\n"));
   console.log(chalk.yellow("Guess the 15 most populous countries with a density of population"));
   console.log(chalk.yellow("\n=================================================================\n"));
 }
 
-
-function runGame() {
+function createRdWord() {
+  var randomWordChoices = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+//Pass random word through word constructor
+var computerWord = new Word(randomWordChoices);
+var requireNewWord = false;
   // Generates new word for Word constructor if true
   if (requireNewWord) {
     // Selects random wordChoices array
-    randomWordChoices = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+    
     // Passes random word through the Word constructor
     computerWord = new Word(randomWordChoices);
+
     requireNewWord = false;
   }
+};
 
 
-  // Check if a letter guessed is correct
+
+function runGame(){
+  createRdWord();
+  // TestS if a letter guessed is correct
   var wordComplete = [];
   computerWord.wordArray.forEach(completeCheck);
 
@@ -70,56 +74,62 @@ function runGame() {
       ])
       .then(function(input) {
         if (
-          //Check if alphabet is not in between A-Z
           !alphabet.includes(input.userinput) ||
           input.userinput.length > 1
         ) {
           console.log(chalk.blue("\n Need to enter a letter between A-Z , Please try again!!\n"));
+          createRdWord();
           runGame();
         } else {
           if (
-            //Check if user guessed the latter that already guessed or user didn't enter any latter
             incorrectLetters.includes(input.userinput) ||
             correctLetters.includes(input.userinput) ||
             input.userinput === ""
           ) {
             console.log(chalk.blue("\nAlready Guessed or Nothing Entered\n"));
+            createRdWord();
             runGame();
           } else {
             // Checks if guess is correct
             var wordCheckArray = [];
+
             computerWord.userGuess(input.userinput);
 
-            // Checks if guess is correct or incorrect
+            // Checks if guess is correct
             computerWord.wordArray.forEach(wordCheck);
             if (wordCheckArray.join("") === wordComplete.join("")) {
               console.log(chalk.red("\nIncorrect\n"));
+
               incorrectLetters.push(input.userinput);
               guessesLeft--;
             } else {
               console.log(chalk.green("\nCorrect!\n"));
+
               correctLetters.push(input.userinput);
             }
 
             computerWord.log();
 
             // Print guesses left
-            console.log("Guesses Left: " + chalk.red(guessesLeft) + "\n");
+            console.log("Guesses Left: " + guessesLeft + "\n");
 
             // Print letters guessed already
             console.log(
-              "Letters Guessed: " + chalk.red(incorrectLetters.join(" "))+ "\n");
+              "Letters Guessed: " + incorrectLetters.join(" ") + "\n"
+            );
 
             // Guesses left
             if (guessesLeft > 0) {
-           //Call to runGame
+              // Call function
+              createRdWord();
               runGame();
             } else {
-              console.log(chalk.red("Sorry, you lose!!\n"));
-
-              console.log(chalk.green("The answer is " + '"'+randomWordChoices+'"\n'));
+              console.log(chalk.green("The answer is " + '"'+randomWordChoices+'"'));
+              console.log(chalk.red("Sorry, you lose!\n"));
               restartGame();
             }
+          
+
             function wordCheck(key) {
               wordCheckArray.push(key.guessed);
             }
@@ -128,6 +138,7 @@ function runGame() {
       });
   } else {
     console.log(chalk.green("YOU WIN!!\n"));
+
     restartGame();
   }
 
@@ -154,6 +165,7 @@ function restartGame() {
         incorrectLetters = [];
         correctLetters = [];
         guessesLeft = 10;
+        createRdWord();
         runGame();;
       } else {
         return;
@@ -161,5 +173,6 @@ function restartGame() {
     });
 }
 startGame();
+createRdWord();
 runGame();
 
